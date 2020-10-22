820feea7fbe1cb65cace88cd2e5c6edc5fd2d690
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2020-10-22 21:17:55 +0300

    make round edges on last task and make red border on checkbox

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,10 +2,20 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
+
+function roundEdges(){
+	$("ul>div>li:last-child").addClass("rounded-bottom").children().addClass("rounded-bottom")
+};
 function railsSort(){
 	$('.sortable').railsSortable({
-	  handle: $(".fa-unsorted").parent()
+	  handle: $(".fa-unsorted").parent(),
+		update: function( event, ui ) {
+			$("ul>div>li").removeClass("rounded-bottom").children().removeClass("rounded-bottom");
+			roundEdges();
+			}
 	});
+	
 };
 
 $(document).ready(railsSort);
+$(document).ready(roundEdges);
