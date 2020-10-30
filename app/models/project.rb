363cdf99edb21e3e20af68f8c6fb7bacd6801887
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2020-10-30 21:00:17 +0200

    add project title validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
 	has_many :tasks,-> { order :position}, dependent: :destroy
 	belongs_to :user
+	validates :title, presence: true
 
 	accepts_nested_attributes_for :tasks
 	default_scope { order "created_at ASC"}
